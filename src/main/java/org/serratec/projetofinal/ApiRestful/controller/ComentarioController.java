cea6c4af23af36e940d2f24b0f1df0e70d6d8416
MariaPaulaBernardino <mariapaulabernardino@hotmail.com>
2023-10-18 15:47:20 -0300

    Commit Maria Paula pacote Controller

--- src/main/java/org/serratec/projetofinal/ApiRestful/controller/ComentarioController.java
+++ src/main/java/org/serratec/projetofinal/ApiRestful/controller/ComentarioController.java
@@ -1,5 +1,70 @@
 package org.serratec.projetofinal.ApiRestful.controller;
 
-public class ComentarioController {
+import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.serratec.projetofinal.ApiRestful.model.Comentario;
+import org.serratec.projetofinal.ApiRestful.repository.ComentarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comentario")
+public class ComentarioController {
+	
+	@Autowired
+    private ComentarioRepository comentarioRepository;
+
+	
+	@GetMapping
+    public ResponseEntity<List<Comentario>> listar() {
+        return ResponseEntity.ok(comentarioRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Comentario> buscar(@PathVariable Long id) {
+        Optional<Comentario> comentarioOpt = comentarioRepository.findById(id);
+        if (comentarioOpt.isPresent()) {
+            return ResponseEntity.ok(comentarioOpt.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Comentario> inserir(@Valid @RequestBody Comentario comentario) {
+    	comentario = comentarioRepository.save(comentario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(comentario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Comentario> atualizar(@PathVariable Long id, @Valid @RequestBody Comentario comentario) {
+        Optional<Comentario> comentarioOpt = comentarioRepository.findById(id);
+        if (comentarioOpt.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        comentario.setId(id);
+        comentarioRepository.save(comentario);
+        return ResponseEntity.ok(comentario);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        Optional<Comentario> comentarioOpt = comentarioRepository.findById(id);
+        if (comentarioOpt.isPresent()) {
+            comentarioRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/org/serratec/projetofinal/ApiRestful/controller/UsuarioController.java
+++ src/main/java/org/serratec/projetofinal/ApiRestful/controller/UsuarioController.java
@@ -1,6 +1,71 @@
 package org.serratec.projetofinal.ApiRestful.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.serratec.projetofinal.ApiRestful.model.Usuario;
+import org.serratec.projetofinal.ApiRestful.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
+	@GetMapping
+    public ResponseEntity<List<Usuario>> listar() {
+        return ResponseEntity.ok(usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscar(@PathVariable Long id) {
+        Optional<Usuario> usuarioOpt = usuarioRepository.findById(id);
+        if (usuarioOpt.isPresent()) {
+            return ResponseEntity.ok(usuarioOpt.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> inserir(@Valid @RequestBody Usuario usuario) {
+    	usuario = usuarioRepository.save(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @Valid @RequestBody Usuario usuario) {
+        Optional<Usuario> usuarioOpt = usuarioRepository.findById(id);
+        if (usuarioOpt.isPresent()) {
+            usuario.setId(id);
+            usuarioRepository.save(usuario);
+            return ResponseEntity.ok(usuario);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        Optional<Usuario> usuarioOpt = usuarioRepository.findById(id);
+        if (usuarioOpt.isPresent()) {
+            usuarioRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
+
